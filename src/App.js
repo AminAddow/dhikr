f28f2c340a70702e92425a3aebd51608989046ff
Amin Addow <amin.addow.96@gmail.com>
2020-04-09 23:19:33 +0000

    Structure and some styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
-import styles from './App.module.css';
+import styles from './styles/App.module.css';
 import Post from './component/post';
 
 function App() {
--- src/component/post.js
+++ src/component/post.js
@@ -1,64 +1,80 @@
 import React, { Component } from 'react';
 import firebase from './../firebase';
-import Dhikr from './dhikr';
+import styles from './../styles/App.module.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
 
 class post extends Component {
-	constructor(props) {
-		super(props);
+    constructor(props) {
+        super(props);
 
-		this.state = {
-			list: []
-		};
-	}
+        this.state = {
+            list: []
+        };
+    }
 
-	getData = () => {
-		// Init connection to firestore
-		var db = firebase.firestore();
+    getData = () => {
+        // Init connection to firestore
+        var db = firebase.firestore();
 
-		// init empty array
-		// var dhikr = [];
+        // init empty array
+        var dhikr = [];
 
-		var dhikr = [];
+        // Retrieve data from firestore
+        db.collection('morning').orderBy("id").get().then((snapshot) => {
+            snapshot.forEach((doc) => {
+                var id = doc.data().id;
+                var source = doc.data().source;
+                var text = doc.data().text;
+                var times_int = doc.data().times_int;
 
-		// Retrieve data from firestore
-		db.collection('morning').get().then((snapshot) => {
-			snapshot.forEach((doc) => {
-				var key = doc.data().key;
-				var text = doc.data().text;
-				var times = doc.data().times;
+                // below can be used to retireve read amount in string
+                // var times_string = doc.data().times_int;
 
-				dhikr.push({ key: key, text: text, times: times });
+                // Push retrieved data to state array
+                dhikr.push({ id: id, text: text, times_int: times_int });
 
-				console.log(' ');
-				console.log(key);
-				console.log(text);
-				console.log(times);
+                console.log(' ');
+                console.log(id);
+                console.log(text);
+                console.log(times_int);
 
-				// Push data to local array
-				// dhikr.push(doc.data().text);
+            });
+            // Send local array to state
+            this.setState({ list: [...this.state.list, ...dhikr] });
+        });
+    };
 
-				// this.setState((prevState) => ({
-				// 	list: {
-				// 		...prevState.list,
-				// 		text: text,
-				// 		times: times
-				// 	}
-				// }));
-			});
-			console.log(dhikr);
-			// Send local array to state
-			this.setState({ list: [ ...this.state.list, ...dhikr ] });
-		});
-	};
+    componentDidMount() {
+        this.getData();
+    }
 
-	componentDidMount() {
-		this.getData();
-	}
-
-	render() {
-		return <div>{<Dhikr listen={this.state.list} />}</div>;
-		// return <div />;
-	}
+    render() {
+        return (
+            <div>
+                {
+                    this.state.list.map(list => (
+                        <>
+                            <Row className={styles.parent}>
+                                <Col className={styles.out} xs={{ span: 10, offset: 1 }} sm={{ span: 8, offset: 2 }}>
+                                    <Row className={styles.in, styles.id}>
+                                        <div>{list.id}</div>
+                                    </Row>
+                                    <Row className={styles.in}>
+                                        <p align="right" dir="rtl" key={list.id}>{list.text}</p>
+                                    </Row>
+                                    <Row className={styles.in, styles.greentext}>
+                                        <p>Read {list.times_int} time</p>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </>
+                    ))
+                }
+            </div>
+        )
+    }
 }
 
 export default post;
