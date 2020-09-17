9352b76308de01ba75e053f6e978ff4bcf415869
Amin Addow <amin.addow.96@gmail.com>
2020-09-17 21:06:01 +0200

    added airtable data

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
-const path = require("path");
-require("dotenv").config();
+const path = require('path');
+require('dotenv').config();
 
 module.exports = {
-  env: {
-    API_URL: process.env.API_URL
-  },
+	env: {
+		API_URL: process.env.API_URL,
+		AIRTABLE_KEY: process.env.AIRTABLE_KEY
+	},
 
-  webpack: (config) => {
-    config.resolve.alias["components"] = path.join(__dirname, "components");
-    config.resolve.alias["public"] = path.join(__dirname, "public");
+	webpack: (config) => {
+		config.resolve.alias['components'] = path.join(__dirname, 'components');
+		config.resolve.alias['public'] = path.join(__dirname, 'public');
 
-    return config;
-  },
+		return config;
+	}
 };
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 import Nav from '../components/nav';
-import firebase from '../firebase';
+import Airtable from 'airtable';
+// import firebase from '../firebase';
 import { useState, useEffect } from 'react';
-import GetMorning from '../components/GetMorning';
+// import GetMorning from '../components/GetMorning';
 
 function IndexPage({ adhkar }) {
 	return (
@@ -10,18 +11,48 @@ function IndexPage({ adhkar }) {
 				<h1 className="text-5xl text-center text-accent-1">Dhikr.life</h1>
 			</div>
 			<div>
-				{/* <ul>
-          {adhkar.map((dhikr) => (
-            <li key={dhikr.id}>{dhikr.text}</li>
-          ))}
-        </ul> */}
+				<ul>{adhkar.map((dhikr) => <li key={dhikr.key_id}>{dhikr.arabic_text}</li>)}</ul>
 			</div>
 		</div>
 	);
 }
 
-// export async function getStaticProps() {
+export async function getStaticProps() {
+	const airtable = new Airtable({
+		apiKey: process.env.AIRTABLE_KEY
+	});
 
-// }
+	const records = await airtable.base('appijFDoXEaa1S4tF')('Data')
+		.select({
+			fields: [
+				'key_id',
+				'dhikr_id',
+				'arabic_text',
+				'read_amount_int',
+				'time_of_day',
+				'source',
+				'translation_eng'
+			],
+			sort: [ { field: 'key_id', direction: 'asc' } ],
+			maxRecords: 3
+		})
+		.all();
+
+	const adhkar = records.map((product) => {
+		return {
+			key_id: product.get('key_id'),
+			dhikr_id: product.get('dhikr_id'),
+			time_of_day: product.get('time_of_day'),
+			arabic_text: product.get('arabic_text'),
+			read_amount_int: product.get('read_amount_int')
+		};
+	});
+
+	return {
+		props: {
+			adhkar
+		}
+	};
+}
 
 export default IndexPage;
