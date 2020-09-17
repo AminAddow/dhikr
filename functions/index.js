4e3338da66006cd364fb732fc2a8abb542b64b0c
Amin Addow <amin.addow.96@gmail.com>
2020-09-17 23:59:11 +0200

    commented cloud func

--- functions/index.js
+++ functions/index.js
@@ -1,50 +1,50 @@
-const functions = require('firebase-functions');
-const fs = require('fs');
+// const functions = require('firebase-functions');
+// const fs = require('fs');
 
-var Airtable = require('airtable');
-Airtable.configure({
-	endpointUrl: 'https://api.airtable.com',
-	apiKey: 'keykwhQIfYJ0rxjnj'
-});
-var base = Airtable.base('appijFDoXEaa1S4tF');
+// var Airtable = require('airtable');
+// Airtable.configure({
+// 	endpointUrl: 'https://api.airtable.com',
+// 	apiKey: ''
+// });
+// var base = Airtable.base('appijFDoXEaa1S4tF');
 
-console.log('Kobling til Airtable: ', base);
+// console.log('Kobling til Airtable: ', base);
 
-var adhkar = [];
-var dhikr;
-base('data')
-	.select({
-		fields: [ 'key_id', 'dhikr_id', 'arabic_text' ],
-		sort: [ { field: 'key_id', direction: 'asc' } ],
-		maxRecords: 3
-	})
-	.firstPage(function(err, records) {
-		if (err) {
-			console.error(err);
-			return;
-		} else {
-			// console.log('Adhkar', records);
-		}
-		records.forEach(function(record) {
-			var key_id = record.get('key_id');
-			var dhikr_id = record.get('dhikr_id');
-			var arabic_text = record.get('arabic_text');
+// var adhkar = [];
+// var dhikr;
+// base('data')
+// 	.select({
+// 		fields: [ 'key_id', 'dhikr_id', 'arabic_text' ],
+// 		sort: [ { field: 'key_id', direction: 'asc' } ],
+// 		maxRecords: 3
+// 	})
+// 	.firstPage(function(err, records) {
+// 		if (err) {
+// 			console.error(err);
+// 			return;
+// 		} else {
+// 			// console.log('Adhkar', records);
+// 		}
+// 		records.forEach(function(record) {
+// 			var key_id = record.get('key_id');
+// 			var dhikr_id = record.get('dhikr_id');
+// 			var arabic_text = record.get('arabic_text');
 
-			dhikr = {
-				key_id: key_id,
-				dhikr_id: dhikr_id,
-				arabic_text: arabic_text
-			};
+// 			dhikr = {
+// 				key_id: key_id,
+// 				dhikr_id: dhikr_id,
+// 				arabic_text: arabic_text
+// 			};
 
-			// var stringed = JSON.stringify(dhikr);
-			adhkar.push(JSON.stringify(dhikr));
-		});
-		// console.log(adhkar);
-		fs.writeFileSync('adhkar.json', adhkar);
-		// if (adhkar.length < 1 || adhkar == undefined) {
-		// 	console.log('Array is empty');
-		// } else {
-		// 	console.log('Array is OK!');
-		// 	fs.writeFileSync('adhkar.json', adhkar);
-		// }
-	});
+// 			// var stringed = JSON.stringify(dhikr);
+// 			adhkar.push(JSON.stringify(dhikr));
+// 		});
+// 		// console.log(adhkar);
+// 		fs.writeFileSync('adhkar.json', adhkar);
+// 		// if (adhkar.length < 1 || adhkar == undefined) {
+// 		// 	console.log('Array is empty');
+// 		// } else {
+// 		// 	console.log('Array is OK!');
+// 		// 	fs.writeFileSync('adhkar.json', adhkar);
+// 		// }
+// 	});
