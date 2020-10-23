7f300f134358319c30617be2585e5704a185d0b8
Amin Addow <amin.addow.96@gmail.com>
2020-10-23 08:19:21 +0200

    Card updates

--- components/adhkar.js
+++ components/adhkar.js
@@ -1,10 +1,26 @@
 import { useState, useEffect } from "react";
 
 export default function adhkar(dhikr) {
-  return (
-          <li className="text-3xl text-right" key={dhikr.data.key_id}>
-            {dhikr.data.arabic_text}
-          </li>
-  );
-}
+  const d = dhikr.data;
 
+  return (
+    <>
+      <div className="flex justify-center items-center bg-gray-100">
+        <div className="rounded sm:w-auto md:w-3/4 lg:1/2
+         overflow-hidden shadow-lg">
+          <div className="rounded-full absolute h-12 w-12 -mt-6 right-0 mr-4 text-4xl bg-gray-400 shadow">
+            <p className="antialiased text-center">{d.dhikr_id}</p>
+          </div>
+          <div className="px-6 pt-8 pb-8">
+            <p className="antialiased text-gray-700 text-3xl rtl">
+              {d.arabic_text}
+            </p>
+          </div>
+          <div>
+            <p>{d.source}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+} 
--- pages/index.js
+++ pages/index.js
@@ -6,58 +6,57 @@ import { useState, useEffect } from "react";
 import Head from "next/head";
 
 function IndexPage({ adhkar }) {
-  return (
-    <div className="flex items-center w-auto">
-      <div>
-        {/* <Landing /> */}
-        <div className="font-arabic flex">
-          {adhkar.map((dhikr) => (
-            <ul>
-              <Adhkar data={dhikr} />
-            </ul>
-          ))}
-        </div>
-      </div>
-    </div>
+	return (
+		<div className="bg-gray-200">
+		{/* <Landing /> */}
+			<div className="font-arabic space-y-10 mt-8 sm:w-auto">
+				{adhkar.map((dhikr) => (
+					<ul>
+					<Adhkar data={dhikr} />
+					</ul>
+				))}
+			</div>
+		</div>
   );
 }
 
 export async function getStaticProps() {
   const airtable = new Airtable({
-    apiKey: process.env.AIRTABLE_KEY,
+	apiKey: process.env.AIRTABLE_KEY,
   });
 
   const records = await airtable
-    .base("appijFDoXEaa1S4tF")("Data")
-    .select({
-      fields: [
-        "key_id",
-        "dhikr_id",
-        "arabic_text",
-        "read_amount_int",
-        "time_of_day",
-        "source",
-        "translation_eng",
-      ],
-      sort: [{ field: "key_id", direction: "asc" }],
-      maxRecords: 1,
-    })
-    .all();
+	.base("appijFDoXEaa1S4tF")("Data")
+	.select({
+	  fields: [
+		"key_id",
+		"dhikr_id",
+		"arabic_text",
+		"read_amount_int",
+		"time_of_day",
+		"source",
+		"translation_eng",
+	  ],
+	  sort: [{ field: "key_id", direction: "asc" }],
+		maxRecords: 5,
+	})
+	.all();
 
   const adhkar = records.map((product) => {
-    return {
-      key_id: product.get("key_id"),
-      dhikr_id: product.get("dhikr_id"),
-      time_of_day: product.get("time_of_day"),
-      arabic_text: product.get("arabic_text"),
-      read_amount_int: product.get("read_amount_int"),
-    };
+	return {
+	  key_id: product.get("key_id"),
+	  dhikr_id: product.get("dhikr_id"),
+	  time_of_day: product.get("time_of_day"),
+	  arabic_text: product.get("arabic_text"),
+	  read_amount_int: product.get("read_amount_int"),
+		source: product.get("source"),
+	};
   });
 
   return {
-    props: {
-      adhkar,
-    },
+	props: {
+	  adhkar,
+	},
   };
 }
 
