a8da38ffcb45fcaf3bff533e956c23edd96820a6
Amin Addow <amin.addow.96@gmail.com>
2020-10-23 17:21:21 +0000

    Card id position

--- components/adhkar.js
+++ components/adhkar.js
@@ -5,14 +5,13 @@ export default function adhkar(dhikr) {
 
   return (
     <>
-      <div className="flex justify-center items-center bg-gray-100">
-        <div className="rounded sm:w-auto md:w-3/4 lg:1/2
-         overflow-hidden shadow-lg">
-          <div className="rounded-full absolute h-12 w-12 -mt-6 right-0 mr-4 text-4xl bg-gray-400 shadow">
-            <p className="antialiased text-center">{d.dhikr_id}</p>
-          </div>
+      <div className="flex justify-center items-center bg-gray-100 sm:w-full md:w-3/4 lg:1/2">
+        <div className="rounded shadow ">
+      <div className="rounded-full h-12 w-12 -mt-6 z-50 right-0 mr-4 text-4xl bg-gray-400 shadow">
+        <p className="antialiased text-center">{d.dhikr_id}</p>
+      </div>
           <div className="px-6 pt-8 pb-8">
-            <p className="antialiased text-gray-700 text-3xl rtl">
+            <p className="antialiased text-gray-700 text-3xl rtl ">
               {d.arabic_text}
             </p>
           </div>
@@ -23,4 +22,4 @@ export default function adhkar(dhikr) {
       </div>
     </>
   );
-} 
+}
