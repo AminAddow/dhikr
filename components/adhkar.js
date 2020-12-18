567c81725496a126de5ea2a5daa50e10badeeb48
Amin Addow <amin.addow.96@gmail.com>
2020-12-18 01:59:14 +0100

    Menu toggle added

--- components/adhkar.js
+++ components/adhkar.js
@@ -3,23 +3,7 @@ import { useState, useEffect } from "react";
 export default function adhkar(dhikr) {
   const d = dhikr.data;
 
-  return (
-    <>
-      {/* Outter Card */}
-      <div className="flex justify-center font-arabic">
-        {/* Inner Card */}
-        <div className="rounded shadow bg-gray-100">
-          {/* Arabic text */}
-          <div className="px-2">
-            <p className="antialiased text-gray-700 text-3xl rtl">
-              {d.arabic_text}
-            </p>
-          </div>
-          <div>
-            <p>{d.source}</p>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+  // font-arabic
+
+  return <div></div>;
 }
