05dc3ed0a92a4354315cb45c2e7896d56d6e1031
Amin Addow <amin.addow.96@gmail.com>
2021-01-12 22:46:35 +0100

    Font added

--- pages/_document.js
+++ pages/_document.js
@@ -4,8 +4,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head></Head>
-        <body>
+        <Head>
+          <link
+            rel="preload"
+            href="/fonts/Scheherazade/Scheherazade-Regular.ttf"
+            as="font"
+            crossOrigin=""
+          />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;500;600&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
+        <body className="font-playfair">
           <Main />
           <NextScript />
         </body>
