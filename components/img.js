30506eec4848344f28bd5319a452549d1ccc00f5
Amin Addow <amin.addow.96@gmail.com>
2021-02-02 00:02:20 +0100

    Done again

--- components/img.js
+++ components/img.js
@@ -1,25 +1,24 @@
 import React from "react";
-import Image from "next/image";
 
 export default function Img(props) {
   return (
     <div className="flex justify-center">
-      <Image
+      {/* <Image
         src="/img/dhikr.png"
         layout="intrinsic"
         width={500}
         height={500}
         alt="Dhikr life logo"
-      />
-      {/* <div className="flex justify-center h-100 w-100">
+      /> */}
+      <div className="flex justify-center h-100 w-100">
         <img
           src="/img/dhikr.png"
           alt="Dhikr life logo"
           layout="intrinsic"
-          width={300}
-          height={300}
+          width={500}
+          height={500}
         />
-      </div> */}
+      </div>
     </div>
   );
 }
