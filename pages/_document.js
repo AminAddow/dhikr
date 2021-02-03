3e4d4e0486876be988e7dc9c8d88332c282e8522
Amin Addow <amin.addow.96@gmail.com>
2021-02-03 06:37:38 +0100

    tag manager added

--- pages/_document.js
+++ pages/_document.js
@@ -2,25 +2,19 @@ import React from "react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 class MyDocument extends Document {
   render() {
-    const GA_MEASUREMENT_ID = "G-17L4DMN49N";
+    const GTM_MEASUREMENT_ID = "GTM-KZ79B24";
     return (
       <Html lang="en">
         <Head>
           <script
-            async
-            src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-          />
-          <script
-            // eslint-disable-next-line react/no-danger
             dangerouslySetInnerHTML={{
-              __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GA_MEASUREMENT_ID}');
-              `,
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+  })(window,document,'script','dataLayer','${GTM_MEASUREMENT_ID}');`,
             }}
-          />
+          ></script>
           <link
             href="/fonts/Uthmanic/KFGQPC.otf"
             as="font"
@@ -34,6 +28,14 @@ class MyDocument extends Document {
           />
         </Head>
         <body className="font-libre">
+          <noscript>
+            <iframe
+              src={`https://www.googletagmanager.com/ns.html?id=${GTM_MEASUREMENT_ID}`}
+              height="0"
+              width="0"
+              style="display:none;visibility:hidden"
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
