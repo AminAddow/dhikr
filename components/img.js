c2bc97191a86eef851e5fcabfa8642a3193ac6a4
Amin Addow <amin.addow.96@gmail.com>
2021-02-08 00:20:21 +0100

    s

--- components/img.js
+++ components/img.js
@@ -4,7 +4,7 @@ export default function Img(props) {
   return (
     <div className="flex justify-center mx-auto py-24 px-6 h-auto w-5/6 md:w-2/3 lg:w-1/2">
       <img
-        className="max-w-max max-h-max"
+        className="max-w-min max-h-min"
         src="/img/dhikr-logo-new.png"
         alt="Dhikr life logo"
       />
