b0d9497b9d293374899476a11214b48ba45389d2
Amin Addow <amin.addow.96@gmail.com>
2021-02-08 00:04:59 +0100

    image set to percent

--- components/img.js
+++ components/img.js
@@ -2,8 +2,12 @@ import React from "react";
 
 export default function Img(props) {
   return (
-    <div className="flex justify-center mx-auto my-24 px-6">
-      <img width="200" src="/img/dhikr-logo-new.png" alt="Dhikr life logo" />
+    <div className="flex justify-center mx-auto my-24 px-6 h-auto w-5/6 md:w-2/3 lg:w-1/2">
+      <img
+        className="w-5/6 h-auto"
+        src="/img/dhikr-logo-new.png"
+        alt="Dhikr life logo"
+      />
     </div>
   );
 }
